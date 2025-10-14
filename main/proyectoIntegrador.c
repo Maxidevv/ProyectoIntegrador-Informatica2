5e05589ad21e208f579766d404b4c8ddbafb5aee
Matias Moyano Bustos <matiasmoyanobustos@gmail.com>
2025-10-14 13:06:09 -0300

    Prueba de archivo

--- main/proyectoIntegrador.c
+++ main/proyectoIntegrador.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main (){
-    printf("Hola Maxi, como tamo");
+void leer_csv(const char *Produccion mundial de litio) {
+    FILE *fp = fopen("Produccion mundial de litio.csv", "r");
+    if (fp == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+
+    char linea[256];
+    fgets(linea, sizeof(linea), fp);
+
+    char pais[64];
+    int anio;
+    float toneladas;
+
+    while (fgets(linea, sizeof(linea), fp)) {
+        if (sscanf(linea, "%63[^;];%d;%f", pais, &anio, &toneladas) == 3) {
+            printf("País: %s, Año: %d, Toneladas: %.6f\n", pais, anio, toneladas);
+        }
+    }
+
+    fclose(fp);
 }
+int main() {
+    leer_csv("main/resources/Produccion mundial de litio.csv");
+    return 0;
+}
+void LitioTotal_anio(){//Esta funcion lo que hace es juntar la cantidad total de litio producido por paises, y ordenalos de mayor a menor(descendente)
 
-
+}
