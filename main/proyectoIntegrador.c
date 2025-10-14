add855a72e28828cf553b1490f47d7054c76dc7a
Maxi <maxilucianomoncada@gmail.com>
2025-10-14 12:37:13 -0300

    Estructura de menu realizada

--- main/proyectoIntegrador.c
+++ main/proyectoIntegrador.c
@@ -1,3 +1,30 @@
-#include <stdio.h>
-#include <stdib.h>
-#include <ctype.h>
+int main() 
+{
+    char eleccionUsuario;
+    FILE *fp;
+
+    printf("Bienvenido al programa.\n");
+    do 
+    {
+        printf("________________________________________________________\n");
+        printf("[A] Leer archivos. \n[B] - Ordenar. \n[C] Imprimir.");
+        printf("________________________________________________________\n");
+        printf("A continuacion ingrese una opcion:\n> ");
+        scanf("%c ", &eleccionUsuario);
+        eleccionUsuario = tolower(eleccionUsuario);
+
+        switch(eleccionUsuario) 
+        {
+            case 'a':
+                break;
+            case 'b':
+                break;
+            case 'c':
+                break;
+            default:
+                printf("La opcion no corresponde.\n");
+        }
+    } while(eleccionUsuario != 's');
+
+    return 0;
+}
